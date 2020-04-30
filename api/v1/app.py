5c6a6a63c138b2af17f9b146a3d9c7d6df5ed853
JamesPagani <jaimeagalvezv@gmail.com>
2020-04-30 04:02:44 +0000

    Upon making an invalid request that returns a HTTP 404 error, display a special JSON response

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """AirBnB clone RESTful API.
-RESTful implementation of the AirBnB clone project.
+RESTful API of the AirBnB clone project.
 """
 
 
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from models import storage
 from os import getenv
 
@@ -14,6 +14,12 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 api_host = getenv("HBNB_API_HOST", default="0.0.0.0")
 api_port = getenv("HBNB_API_PORT", default="5000")
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Special HTTP 404 handler."""
+    return make_response(jsonify(error="Not found"), 404)
 
 
 @app.teardown_appcontext
